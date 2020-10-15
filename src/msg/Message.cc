b7d58c26125b1ac12c73be2467fda61f8befeb58
Changcheng Liu <changcheng.liu@aliyun.com>
2020-10-15 10:13:41 +0800

    msg/messages: enable decode MReplicaDaemonMap message
    Signed-off-by: Changcheng Liu <changcheng.liu@aliyun.com>

--- src/msg/Message.cc
+++ src/msg/Message.cc
@@ -216,6 +216,8 @@
 #include "messages/MOSDPGUpdateLogMissing.h"
 #include "messages/MOSDPGUpdateLogMissingReply.h"
 
+#include "messages/MReplicaDaemonMap.h"
+
 #ifdef WITH_BLKIN
 #include "Messenger.h"
 #endif
@@ -915,6 +917,12 @@ Message *decode_message(CephContext *cct,
   case MSG_MON_HEALTH_CHECKS:
     m = make_message<MMonHealthChecks>();
     break;
+
+#if defined(WITH_CACHE_REPLICA)
+  case CEPH_MSG_REPLICADAEMON_MAP:
+    m = make_message<MReplicaDaemonMap>();
+    break;
+#endif
 
     // -- simple messages without payload --
 
